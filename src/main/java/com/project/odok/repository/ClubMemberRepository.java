2daf4f994197489b165a12090cf5c8a7358238e5
atto08 <atto08@naver.com>
2022-11-18 00:49:30 +0900

    Modify The whole Club

--- src/main/java/com/project/odok/repository/ClubMemberRepository.java
+++ src/main/java/com/project/odok/repository/ClubMemberRepository.java
@@ -1,7 +1,10 @@
 package com.project.odok.repository;
 
+import com.project.odok.entity.Club;
 import com.project.odok.entity.ClubMember;
+import com.project.odok.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+    Boolean existsByMemberAndClub(Member member, Club club);
 }
