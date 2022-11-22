00ccb24f94e53dbc944072f59bfe17805cf6c377
ryucm <ckd12394@gmail.com>
2022-11-22 16:27:09 +0900

    Add create Bean in RedisConfig and modify workflow

--- src/main/java/com/project/odok/config/RedisConfig.java
+++ src/main/java/com/project/odok/config/RedisConfig.java
@@ -1,13 +1,12 @@
 package com.project.odok.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -31,5 +30,17 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+
+    public RedisMessageListenerContainer RedisMessageListener(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+
     }
 }
