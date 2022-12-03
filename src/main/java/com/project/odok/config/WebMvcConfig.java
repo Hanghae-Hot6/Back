6aae6bcd95e303e15a8cc7a7339f47e44aa082dd
ryucm <ckd12394@gmail.com>
2022-12-03 16:31:18 +0900

    Modify CORS

--- src/main/java/com/project/odok/config/WebMvcConfig.java
+++ src/main/java/com/project/odok/config/WebMvcConfig.java
@@ -10,7 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://hot6-front.s3-website.ap-northeast-2.amazonaws.com/")
+                .allowedOrigins("http://localhost:3000",
+                        "http://hot6-front.s3-website.ap-northeast-2.amazonaws.com/",
+                        "https://www.o-dok.com",
+                        "https://o-dok.com")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .exposedHeaders("Authorization","Refresh-Token")
                 .allowCredentials(true)
